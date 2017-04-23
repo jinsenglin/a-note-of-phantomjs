e652fdcf6918a402bb4401deff4983dd12d016a5
Jim Lin <jimlintw922@gmail.com>
2017-04-23 11:14:48 +0800

    remove capturejs

--- capturejs/README.md
+++
@@ -1,1 +0,0 @@
-capturejs --uri https://www.google.com --output google.com.png --javascript-file ./inputsearch.js
--- capturejs/inputsearch.js
+++
@@ -1,3 +0,0 @@
-var form = document.forms[0];
-var selectElement = form.querySelector('input[name="q"]');
-selectElement.value = "phantomjs";
