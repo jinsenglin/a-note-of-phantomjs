d850bff3303e5b144d57f549dcd9452da7a2338a
Jim Lin <jimlintw922@gmail.com>
2017-04-26 17:13:01 +0800

    update casperjs

--- examples/casperjs/form-post.js
+++ examples/casperjs/form-post.js
@@ -1,62 +1,3 @@
-# Navigate and Capture
-
-Install
-
-```
-npm install -g casperjs
-```
-
-[quick-start.js](examples/casperjs/quick-start.js)
-
-```
-var casper = require('casper').create();
-casper.start('http://casperjs.org/');
-
-casper.then(function() {
-	this.echo('First Page: ' + this.getTitle());
-	this.capture('page1.png', {
-		     top: 0,
-		     left: 0,
-		     width: 1024,
-		     height: 768
-	});
-});
-
-casper.thenOpen('http://phantomjs.org', function() {
-	this.echo('Second Page: ' + this.getTitle());
-	this.capture('page2.png', {
-		top: 0,
-		left: 0,
-		width: 1024,
-		height: 768
-	});
-});
-
-casper.run();
-```
-
-Run
-
-```
-casperjs quick-start.js
-```
-
-My Versions
-* nodejs 4.8.0
-* phantomjs 2.1.1
-* casperjs 1.1.3
-
-Methods
-* start http://docs.casperjs.org/en/latest/modules/casper.html#start
-* open http://docs.casperjs.org/en/latest/modules/casper.html#open
-* capture http://docs.casperjs.org/en/latest/modules/casper.html#capture
-* run http://docs.casperjs.org/en/latest/modules/casper.html#run
-
----
-
-[form-post.js](examples/form-post.js)
-
-```
 var casper = require('casper').create();
 casper.start().thenOpen('http://localhost/form/handler/api', {
 	method: 'post',
@@ -71,4 +12,3 @@ casper.start().thenOpen('http://localhost/form/handler/api', {
 });
 
 casper.run();
-```
